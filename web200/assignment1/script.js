28bc10dcd821e4574dba2d66b332d518b3f0c4cd
Eric <sharkym02@gmail.com>
2024-01-21 09:53:36 -0600

    Finished Assignment 1

---
+++ web200/assignment1/script.js
@@ -0,0 +1,10 @@
+<script>
+    // this function changes the text of one of the paragraphs on button click
+    function getElement() {
+        element = document.getElementById("getElement").innerText = "Hello Eric!";
+    }
+    // This function outputs text using the console log
+    function console() {
+        console.log("Hello Eric!");
+    }
+</script>
