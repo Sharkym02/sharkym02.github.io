879df79280b86a1a5671152486f7e3f62a070d1b
Eric <sharkym02@gmail.com>
2024-01-25 17:51:54 -0600

    fix script file

--- web200/assignment1/script.js
+++ web200/assignment1/script.js
@@ -1,10 +1,8 @@
-<script>
-    // this function changes the text of one of the paragraphs on button click
-    function getElement() {
-        element = document.getElementById("getElement").innerText = "Hello Eric!";
-    }
-    // This function outputs text using the console log
-    function console() {
-        console.log("Hello Eric!");
-    }
-</script>
+// this function changes the text of one of the paragraphs on button click
+function getElement() {
+    element = document.getElementById("getElement").innerText = "Hello Eric!";
+}
+// This function outputs text using the console log
+function console() {
+    console.log("Hello Eric!");
+}
