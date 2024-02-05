8a4107ec9a2ce0fa1d93b2b696415a559dbf3d2e
Eric <sharkym02@gmail.com>
2024-02-05 15:03:02 -0600

    gross pay test 1

--- web200/assignment3 copy/script.js
+++
@@ -1,8 +0,0 @@
-// this function changes the text of one of the paragraphs on button click
-function getElement() {
-    element = document.getElementById("getElement").innerText = "Hello Eric!";
-}
-// This function outputs text using the console log
-function consoleOut() {
-    console.log("Hello Eric!");
-}
