710b900092ce4eb134d9b85f2ba0712998f6dcbb
Eric <sharkym02@gmail.com>
2024-02-14 20:29:38 -0600

    took an L off of dollars

--- web200/assignment4/script.js
+++ web200/assignment4/script.js
@@ -1,10 +1,13 @@
+
 function CalculatePay() {
     var hours = document.getElementById("hours").value;
     var rate = document.getElementById("rate").value;
     var weekly = hours * rate;
     var monthly = weekly * 4;
     var annual = monthly * 13;
+    //window.alert("You make " + weekly + " dollars a week, " + monthly + " dollars a month, and " + annual + " dollars a year.")
     element = document.getElementById("GrossPay").innerText = "You make " + weekly + " dollars a week, " + monthly + " dollars a month, and " + annual + " dollars a year.";
+    //document.write("You make " + weekly + " dollars a week, " + monthly + " dollars a month, and " + annual + " dollars a year.")
 }
 function CalculateAge() {
     var age = document.getElementById("age").value;
@@ -12,16 +15,18 @@ function CalculateAge() {
     var days = months * 31;
     var hours = days * 24;
     var seconds = hours * 3600;
+    //window.alert("You make " + weekly + " dollars a week, " + monthly + " dollars a month, and " + annual + " dollars a year.")
     element = document.getElementById("ageCalc").innerText = "You are " + age + " year(s) old, " + months + " months old, " + days + " days old, " + hours + " hours old, and " + seconds + " seconds old.";
+    //document.write("You make " + weekly + " dollars a week, " + monthly + " dollars a month, and " + annual + " dollars a year.")
 }
 function CalculatePaint() {
-    var length = window.prompt("What is the Length of the room in feet");
-    var width = window.prompt("What is the Width of the room in feet");
-    var height = window.prompt("What is the Height of the room in feet");
-    var cover = window.prompt("How many square feet will a gallon of paint cover");
-    var cost = window.prompt("How expensive is a gallon of paint");
+    var length = document.getElementById("Length").value;
+    var width = document.getElementById("Width").value;
+    var height = document.getElementById("Height").value;
+    var cover = document.getElementById("cover").value;
+    var cost = document.getElementById("cost").value;
     area = 2 * length * height + 2 * width * height;
     gallons = Math.ceil(area / cover);
     totalCost = gallons * cost;
-    window.alert("You need " + gallons + " gallons of paint, and it will cost " + totalCost + " dollars")
+    element = document.getElementById("paint").innerText = "You need " + gallons + " gallons of paint, and it will cost " + totalCost + " dollars";
 }
