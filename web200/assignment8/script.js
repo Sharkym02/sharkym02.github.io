e504d892acc81f028b9c8a15da275694a6e368a2
Eric <sharkym02@gmail.com>
2024-03-15 00:58:41 -0500

    semicolon moment

--- web200/assignment8/script.js
+++ web200/assignment8/script.js
@@ -2,7 +2,7 @@ function calculateAverage() {
     var number = document.getElementById("number").value;
     var values = [];
     for (let i = 0; i < number; i++) {
-            p = i+1
+            p = i+1;
             values[i] == parseInt(prompt("What is score number " + p + "?"));
     }
     document.getElementById("scores").innerText = values.reduce((a, b) => a + b, 0) / number;
