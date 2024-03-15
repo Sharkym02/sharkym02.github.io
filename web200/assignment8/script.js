5ca816cf475bbb127fe0a51d25518d941e9c0ee0
Eric <sharkym02@gmail.com>
2024-03-14 23:44:42 -0500

    fix script file in assignment 8

--- web200/assignment8/script.js
+++ web200/assignment8/script.js
@@ -1,19 +1,9 @@
-function mathExpressions()  {
-    var value = document.getElementById("value").value;
-    var expressions = document.getElementById("expressions").value;
-    output = "";
-    i=1;
-    while (i <= expressions) {
-        output += value + " * " + i + " = " + value * i + "\n";
-        i++;
-    }
-    document.getElementById("math").innerText = output;
-}
 function calculateAverage() {
     var number = document.getElementById("number").value;
-    total = 0;
-    for (let i = 1; i <= number; i++) {
-        total += parseInt(prompt("What is score number " + i + "?"));
+    var values = [];
+    for (let i = 0; i < number; i++) {
+            p = i+1
+            values[i] == parseInt(prompt("What is score number " + p + "?"));
     }
-    document.getElementById("scores").innerText = total / number;
+    document.getElementById("scores").innerText = values.reduce((a, b) => a + b, 0) / number;
 }
